80969bfaeceb770f1b9e240fa0177ca59636c8bf
Nate Hermes <natedog55902@yahoo.com>
2026-04-24 17:44:08 -0500

    changes

--- tools/Pokemon.py
+++ tools/Pokemon.py
@@ -1,4 +1,14 @@
 class Pokemon:
-    def __init__(self, name, number):
-        self.name = name
-        self.dexNum = number
+    numspecialties = 2
+    numfavorites = 6
+
+    def __init__(self, data):
+        self.name = data['Name']
+        self.dexNum = data['Number']
+        self.habitat = data["Ideal Habitat"]
+        self.specialties = []
+        for s in range(self.numspecialties):
+            self.specialties.append(data[f"Specialty {s+1}"])
+        self.favorites = []
+        for f in range(self.numfavorites):
+            self.favorites.append(data[f"Favorite {f+1}"])
--- tools/searcher.py
+++ tools/searcher.py
@@ -3,12 +3,16 @@ import csv
 import Pokemon
 
 pokemon = []
+categories = ["Blocky stuff","Glass stuff","Luxury","Strange stuff","Watching stuff"]
 with open('Pokopia.csv', mode='r', newline='') as file:
     reader = csv.DictReader(file)
     for row in reader:
         # print(row["Name"])
-        p = Pokemon.Pokemon(row["Name"],row["Number"])
-        print(f"Adding {p.name} ({p.dexNum})")
+        p = Pokemon.Pokemon(row)
+        # print(f"Adding {p.name} ({p.dexNum}) ({p.specialties}) ({p.favorites})")
         pokemon.append(p)
+        if "Trade" in p.specialties:
+            print(p.name)
 
-print(f"\n{len(pokemon)} total Pokemon")
+# print(f"\n{len(pokemon)} total Pokemon")
+# print(f"Each Pokemon has {Pokemon.Pokemon.numfavorites} favorites")
