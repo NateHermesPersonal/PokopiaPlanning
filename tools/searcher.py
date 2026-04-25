8419f4b7e9fb06635e01cb5a7445ebd211cb167d
Nate Hermes <natedog55902@yahoo.com>
2026-04-25 10:43:02 -0500

    Update searcher.py

--- tools/searcher.py
+++ tools/searcher.py
@@ -3,16 +3,23 @@ import csv
 import Pokemon
 
 pokemon = []
-categories = ["Blocky stuff","Glass stuff","Luxury","Strange stuff","Watching stuff"]
+categories = ["Blocky stuff","Glass stuff","Luxury","Strange stuff","Watching stuff"] # Wall Mirror
 with open('Pokopia.csv', mode='r', newline='') as file:
     reader = csv.DictReader(file)
     for row in reader:
         # print(row["Name"])
         p = Pokemon.Pokemon(row)
         # print(f"Adding {p.name} ({p.dexNum}) ({p.specialties}) ({p.favorites})")
-        pokemon.append(p)
         if "Trade" in p.specialties:
-            print(p.name)
+            # print(p.name)
+            for c in categories:
+                if c in p.favorites:
+                    print(f"{p.name} ({c})")
+                    pokemon.append(p)
+                    break
+            # else:
+            #     print(f"{p.name} doesn't have a favorite of any of the categories")
+print(f"{len(pokemon)} total compatible traders")
 
 # print(f"\n{len(pokemon)} total Pokemon")
 # print(f"Each Pokemon has {Pokemon.Pokemon.numfavorites} favorites")
