841a3bd9ee46aa64f727d50c5daa1381f25d2114
Nate Hermes <natedog55902@yahoo.com>
2026-04-27 07:36:08 -0500

    Update searcher.py

--- tools/searcher.py
+++ tools/searcher.py
@@ -4,6 +4,7 @@ import Pokemon
 
 pokemon = []
 categories = ["Blocky stuff","Glass stuff","Luxury","Strange stuff","Watching stuff"] # Wall Mirror
+habitats = {}
 with open('Pokopia.csv', mode='r', newline='') as file:
     reader = csv.DictReader(file)
     for row in reader:
@@ -14,12 +15,18 @@ with open('Pokopia.csv', mode='r', newline='') as file:
             # print(p.name)
             for c in categories:
                 if c in p.favorites:
-                    print(f"{p.name} ({c})")
+                    h = p.habitat
+                    if h not in habitats.keys():
+                        habitats[h] = 1
+                    else:
+                        habitats[h] += 1
+                    print(f"{p.name}[{h}] ({c})")
                     pokemon.append(p)
                     break
             # else:
             #     print(f"{p.name} doesn't have a favorite of any of the categories")
 print(f"{len(pokemon)} total compatible traders")
+print(habitats)
 
 # print(f"\n{len(pokemon)} total Pokemon")
 # print(f"Each Pokemon has {Pokemon.Pokemon.numfavorites} favorites")
